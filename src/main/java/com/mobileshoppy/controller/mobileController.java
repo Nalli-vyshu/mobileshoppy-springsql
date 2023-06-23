4995377f8eb73851783a3baee69eb2ac855e7488
Nalli-vyshu <nallivyshnavi7@gmail.com>
2023-06-23 12:08:50 +0530

    spring boot 2

--- src/main/java/com/mobileshoppy/controller/mobileController.java
+++ src/main/java/com/mobileshoppy/controller/mobileController.java
@@ -1,8 +1,14 @@
 package com.mobileshoppy.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,5 +33,20 @@ public class mobileController {
 	@RequestMapping(method=RequestMethod.GET,value="/mobile")
       public List<mobile> getMobile() {
     	  return service.getMobile();
+      }
+      
+      @PutMapping("/updatemobile")
+      
+      public String updatemobile(@RequestBody mobile m2) {
+    	  return service.updateMobile(m2);
+    }
+      @DeleteMapping("/mobile/{id}")
+      public String deleteMobile(@PathVariable int id) {
+    	  return service.deleteMobile(id);
+    	  
+      }
+      @GetMapping("mobile/{id}")
+      public Optional<mobile> getMobilebyId(@PathVariable int id) {
+    	  return service.getMobilebyId(id);
       }
 }
--- src/main/java/com/mobileshoppy/service/mobileService.java
+++ src/main/java/com/mobileshoppy/service/mobileService.java
@@ -24,8 +24,33 @@ public class mobileService {
 		  repo.findAll().forEach(mobilelist::add);
 		  return mobilelist;
 		  
-		  
+	 }
+	  
+	  public String updateMobile(mobile m2) {
+		 mobile m= repo.save(m2);
+		   if(m!=null) {
+			      return "mobile update";
+		 }
+		   else {
+			      return null;
+		   }
 		  
 	  }
 
+	    public String deleteMobile(int mobileid) {
+	    	
+	    	Optional<mobile> m1=repo.findById(mobileid);
+	    	
+	    	if(m1==null) {
+	    		return null;
+	    	}
+	    	else {
+	    		repo.deleteById(mobileid);
+	    		return "mobile deleted";
+	    	}
+	    	
+	    }
+	    public Optional<mobile> getMobilebyId(int id) {
+	    	return repo.findById(id);
+	    }
 }
